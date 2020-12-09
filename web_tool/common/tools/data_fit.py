6b9dfa51be9ac41fa5c2658b5f2b2188220553a9
githmy <1272736235@qq.com>
2020-12-10 00:08:23 +0800

    v0.004

--- web_tool/common/tools/data_fit.py
+++ web_tool/common/tools/data_fit.py
@@ -11,6 +11,21 @@ from sklearn.pipeline import Pipeline
 from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
 from pprint import pprint
+import random, os
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm  # 最小二乘
+from statsmodels.formula.api import ols  # 加载ols模型
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import Pipeline
+from sklearn.datasets import make_regression
+from sklearn.model_selection import train_test_split
+from pprint import pprint
+import json
+import datetime
+import itertools
+import re
 import time
 import json
 import datetime
@@ -88,6 +103,36 @@ def iter_regression4allxy(t_pandas, max_combnum=2, test_size=0.2):
                     print(datetime.datetime.today())
     return fit_json
 
+def tcn():
+    # https://www.cnpython.com/pypi/keras-tcn
+    batch_size, timesteps, input_dim = None, 20, 1
+
+    def get_x_y(size=1000):
+        pos_indices = np.random.choice(size, size=int(size // 2), replace=False)
+        x_train = np.zeros(shape=(size, timesteps, 1))
+        y_train = np.zeros(shape=(size, 1))
+        x_train[pos_indices, 0] = 1.0
+        y_train[pos_indices, 0] = 1.0
+        return x_train, y_train
+
+    i = Input(batch_shape=(batch_size, timesteps, input_dim))
+
+    o = tcn.TCN(i, return_sequences=False)  # regression problem here.
+    o = Dense(1)(o)
+
+    m = Model(inputs=[i], outputs=[o])
+    m.compile(optimizer='adam', loss='mse')
+
+    x, y = get_x_y()
+    m.fit(x, y, epochs=10, validation_split=0.2)
+
+    o = tcn.TCN(return_sequences=True)(i)
+    o = tcn.TCN(return_sequences=False)(o)
+    # 已有模型
+    model = tcn.compiled_tcn(...)
+    model.fit(x, y)  # Keras model.
+    print(99999)
+
 
 def main():
     # 构造变量, 数据量小于150，预测准确率加速降低。
@@ -113,4 +158,6 @@ def main():
 
 
 if __name__ == '__main__':
+    tcn()
+    exit()
     main()
